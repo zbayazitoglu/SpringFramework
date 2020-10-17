7a25545e5447c90c2337ea69a78b6ab6ea9e94c0
zbayazitoglu <zbayazitoglu@gmail.com>
2020-10-17 19:51:43 +0100

    first commit

---
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -0,0 +1,17 @@
+import implementation.Mentor;
+import service.FullTimeMentor;
+import service.MentorAccount;
+import service.PartTimeMentor;
+
+public class CybertekApp {
+
+    public static void main(String[] args) {
+
+        //FullTimeMentor fullTime = new FullTimeMentor();
+        PartTimeMentor partTime = new PartTimeMentor();
+
+        MentorAccount mentor = new MentorAccount(partTime);
+
+        mentor.manageAccount();
+    }
+}
